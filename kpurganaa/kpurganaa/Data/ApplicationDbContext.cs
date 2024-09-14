1427d4e33623a3640929d4aa1311934605fcae8d
LuisMunar <fernandoparrarengifo20@gmail.com>
2024-09-14 13:18:19 -0500

    cambios 2

---
+++ kpurganaa/kpurganaa/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using kpurganaa.Models; // Asegúrate de que este namespace sea correcto
+
+namespace kpurganaa.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Reserva> Reservas { get; set; }
+    }
+}
--- kpurganaa/kpurganaa/obj/Debug/net6.0/kpurganaa.AssemblyInfo.cs
+++ kpurganaa/kpurganaa/obj/Debug/net6.0/kpurganaa.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("kpurganaa")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a3193a243e37aaeeca825ea327468ac4479f758")]
 [assembly: System.Reflection.AssemblyProductAttribute("kpurganaa")]
 [assembly: System.Reflection.AssemblyTitleAttribute("kpurganaa")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
